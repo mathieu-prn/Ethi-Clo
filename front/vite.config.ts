80a4c8163ffb8075bf977b272a0bbb734e297802
Mathieu Prince <mathieu1.prince1@gmail.com>
2026-04-01 16:37:09 +0200

    Revert "moved to https run dev"
    This reverts commit 6e18ea59d203168d2cca9834f827f5a8a86ecf93.

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    basicSsl()
-  ],
-  server: {
-    host: true // This allows your phone to connect
-  }
+  plugins: [react()],
 })
