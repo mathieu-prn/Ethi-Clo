91f0ff8dd52a7346713b48d0bce6b88f622ade97
Mathieu Prince <mathieu1.prince1@gmail.com>
2026-05-22 11:38:42 +0200

    tried to deploy as a PWA (didn't work :c)

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -4,6 +4,10 @@ import './styles/index.css'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js");
+}
+
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
